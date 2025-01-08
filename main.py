6fb114aa9144aaeb6aeeb3cfa939619780e1fa9b
kimkangmin527 <gimgangmin363@gmail.com>
2025-01-08 11:34:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ else:
 
 if st.button("Aloha", type="tertiary"):
     st.write("Ciao")
+import streamlit as st
+
+left, middle, right = st.columns(3)
+if left.button("Plain button", use_container_width=True):
+    left.markdown("You clicked the plain button.")
+if middle.button("Emoji button", icon="😃", use_container_width=True):
+    middle.markdown("You clicked the emoji button.")
+if right.button("Material button", icon=":material/mood:", use_container_width=True):
+    right.markdown("You clicked the Material button.")
